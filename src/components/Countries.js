d12c5e338e9558815ddf092e1b31528443a21d93
DeepakMishraDA <deepakvinodmishra@gmail.com>
2021-12-01 03:36:15 +0100

    commit

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -1,14 +1,14 @@
 import React from "react";
 import TableContainer from "@material-ui/core/TableContainer";
-import TableHead from "@material-ui/core/TableHead";
+//import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
 import Table from "@material-ui/core/Table";
 import TableCell from "@material-ui/core/TableCell";
 //import { makeStyles } from "@material-ui/core";
 import { useState, useEffect } from "react";
 import axios from "axios";
-import Paper from "@material-ui/core/Paper";
-import TableBody from "@material-ui/core/TableBody";
+//import Paper from "@material-ui/core/Paper";
+//import TableBody from "@material-ui/core/TableBody";
 // const useStyles = makeStyles({
 //   tableStyle: {
 //     width: "960px",
@@ -25,14 +25,14 @@ import TableBody from "@material-ui/core/TableBody";
 function Countries() {
   //const classes = useStyles();
   const [countries, setCountry] = useState([]);
-  console.log(countries);
+  //console.log(countries);
   // useEffect(() => {
   //   console.log("I am useEffect");
   // }, [countries]);
   useEffect(() => {
     const fetchData = async () => {
       const responses = await axios.get("https://restcountries.com/v3.1/all");
-      console.log(responses.data[0].population);
+      console.log(responses.data);
       //const data = JSON.parse(responses);
       setCountry(responses.data);
     };
@@ -41,37 +41,34 @@ function Countries() {
 
   return (
     <div>
-      {countries.map((item) => {
-        return (
-          <TableContainer component={Paper}>
-            <Table sx={{ minWidth: 550 }} aria-label="simple table">
-              <TableHead>
+      <TableContainer>
+        <div>
+          <Table>
+            <TableRow>
+              <TableCell>Flags</TableCell>
+              <TableCell>Countries</TableCell>
+              <TableCell>Population</TableCell>
+              <TableCell>Capital</TableCell>
+              <TableCell>Continents</TableCell>
+            </TableRow>
+          </Table>
+        </div>
+        <div>
+          <Table>
+            {countries.map((data) => {
+              return (
                 <TableRow>
-                  <TableCell>Population</TableCell>
-                  <TableCell align="left">Countries</TableCell>
-                  <TableCell align="left">Currencies</TableCell>
-                  <TableCell align="left">Continents</TableCell>
+                  <TableCell>{data.flag}</TableCell>
+                  <TableCell>{data.name.common}</TableCell>
+                  <TableCell>{data.population}</TableCell>
+                  <TableCell>{data.capital}</TableCell>
+                  <TableCell>{data.continents[0]}</TableCell>
                 </TableRow>
-              </TableHead>
-              <TableBody>
-                {countries.map((row) => (
-                  <TableRow
-                    key={row}
-                    sx={{ "&:last-child td, &:last-child th": { border: 10 } }}
-                  >
-                    <TableCell component="th" scope="row">
-                      {row.population}
-                    </TableCell>
-                    <TableCell align="left">{row.name.common}</TableCell>
-                    <TableCell align="left">{row.currencies}</TableCell>
-                    <TableCell align="left">{row.region}</TableCell>
-                  </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </TableContainer>
-        );
-      })}
+              );
+            })}
+          </Table>
+        </div>
+      </TableContainer>
     </div>
   );
 }
