3663e2c0eab71117632ffed1b463b16a22c52d71
DeepakMishraDA <deepakvinodmishra@gmail.com>
2021-12-02 04:53:12 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
 import Country from "./components/Country";
+import Countries from "./components/Countries";
+
 function App() {
   return (
     <div className="App">
-      <Country />
+      <Routes>
+        <Route path="/" element={<Countries />} />
+        <Route path="about/" element={<Country />} />
+      </Routes>
     </div>
   );
 }
