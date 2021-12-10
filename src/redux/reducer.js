7e3a6384cf69d20fcaea00f6f26b57dbe3532adb
DeepakMishraDA <deepakvinodmishra@gmail.com>
2021-12-10 17:27:07 +0100

    commit

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,14 +1,30 @@
 const defaultState = {
-  count: [],
+  cart: [],
 };
 
 const countReducer = (state = defaultState, action) => {
   switch (action.type) {
-    case "Increase":
-      console.log(action.payload);
-      break;
+    case "INSERT_COUNTRY":
+      const incomingCountry = action.payload;
+      const incomingcountryName = incomingCountry.name;
+      return {
+        const existCountry = state.cart.find(country=> {
+          if (incomingcountryName ===country.name){
+            return true
+          }
+          return false
+        })
+        if (existCountry){
+          return state
+        } else{
+          return ({
+            ...state,
+            cart:[...state.cart,incomingCountry]
+          })
+        }
+      }
 
-    case "Decrease":
+    case "REMOVE_COUNTRY":
       return {
         ...state,
         count: state.count - 1,
